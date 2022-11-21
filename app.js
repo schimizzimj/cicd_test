df457f0ce9c9d42e326134a1711cd5b65194b400
Marcus Schimizzi <marcusjschimizzi@gmail.com>
2022-11-21 14:24:12 -0800

    add random endpoint

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/random", function (req, res) {
+  res.send({
+    value: Math.random(),
+  });
+});
+
 app.get("/", function (req, res) {
   res.send("yo");
 });
--- test/test.js
+++ test/test.js
@@ -1,8 +1,21 @@
+const { expect } = require("chai");
 const request = require("supertest");
 const app = require("../app.js");
 
 describe("GET /", function () {
   it("should respond with hello world", function (done) {
     request(app).get("/").expect("yo", done);
+  });
+});
+
+describe("GET /random", function () {
+  it("should respond with a random number betwen 0 and 1", function (done) {
+    request(app).get("/random").expect(expectNumber).end(done);
+
+    function expectNumber(res) {
+      const value = parseFloat(res.body.value);
+      expect(value).is.greaterThanOrEqual(0);
+      expect(value).is.lessThanOrEqual(1);
+    }
   });
 });
